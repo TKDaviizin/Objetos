4f663fedd5b5a3502c68723968e12bf360a93f8c
TKDaviizin <gamerspc83@gmail.com>
2025-04-09 16:41:51 +0000

    pronto

---
+++ 1.js
@@ -0,0 +1,7 @@
+const endereco = {
+        rua: "Rua Jaime Oliveira Novais",
+        cep: "46500-000",
+        cidade:"Macaúbas",
+        estado:"Bahia",
+    }
+console.log(endereco.rua)
---
+++ objetos.js
@@ -0,0 +1,8 @@
+const pessoa={
+    nome: "Davi",
+    idade: 18,
+    endereco:{
+        rua: "Rua Jaime Oliveira Novais",
+        cep: "46500-000"
+    }
+}
